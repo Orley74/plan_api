5eb0eba8fda52c5d826afe022926176caa438681
Orley74 <kacper.chwaszcz@vp.pl>
2023-10-23 14:02:45 +0200

    a

--- proj/settings.py
+++ proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mx9sihquf$l&1jm)d8x(e=6iitqv(4$u@22pgdoef^s4r(^fxg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
