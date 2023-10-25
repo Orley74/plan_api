de022376cdce299799fc43bf821f193b228e2f57
Orley74 <kacper.chwaszcz@vp.pl>
2023-10-25 11:25:58 +0200

    v0.9

--- proj/deployment.py
+++ proj/deployment.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+DISABLE_HUGO_BUILD = True
 
 # conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 # conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
