56c3bc00be453e503b5af61152cdfe02658491c4
Orley74 <kacper.chwaszcz@vp.pl>
2023-10-25 15:35:37 +0200

    asd

--- proj/deployment.py
+++ proj/deployment.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = ['planapi.azurewebsites.net']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 
 DEBUG = False
 
