2838f7c7388a2838b16bbd01fde62c290472ac82
Orley74 <kacper.chwaszcz@vp.pl>
2023-10-25 13:41:35 +0200

    asd

--- proj/deployment.py
+++ proj/deployment.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['*']
 
 DEBUG = False
 
