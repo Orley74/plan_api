fd36a695bd2b7f432dac95db1919cc48fa967529
Orley74 <117045351+Orley74@users.noreply.github.com>
2024-01-25 16:22:42 +0000

    a

--- Scraper/Scraper/pipelines.py
+++ Scraper/Scraper/pipelines.py
@@ -12,37 +12,37 @@ from neo4j import GraphDatabase, RoutingControl, exceptions
 from itemadapter import ItemAdapter
 
 
-# class JsonWriterPipeline:
-#     uri = 'neo4j+s://b35e138c.databases.neo4j.io'
-#     auth = ("neo4j", 'VGfvQTk0VCkEzne79CGPXTKA_Eykhx0OwudLZUKG7sQ')
-
-#     def open_spider(self, spider):
-#         self.driver = GraphDatabase.driver(self.uri, auth= self.auth)
-
-
-#     def close_spider(self, spider):
-#         self.driver.close()
-    
-
-#     def process_item(self, item, spider):
-      
-#         self.driver.execute_query(
-#         "Create (p:asdasd) ",
-#         database_="neo4j")
-#         # line = json.dumps(ItemAdapter(item).asdict()) + "\n"
-#         # print(line)
-#         return item
-
-
-
 class JsonWriterPipeline:
+    uri = 'neo4j+s://b35e138c.databases.neo4j.io'
+    auth = ("neo4j", 'VGfvQTk0VCkEzne79CGPXTKA_Eykhx0OwudLZUKG7sQ')
+
     def open_spider(self, spider):
-        self.file = open("items.jsonl", "w")
+        self.driver = GraphDatabase.driver(self.uri, auth= self.auth)
+
 
     def close_spider(self, spider):
-        self.file.close()
+        self.driver.close()
+    
 
     def process_item(self, item, spider):
-        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
-        self.file.write(line)
+      
+        self.driver.execute_query(
+        "Create (p:asdasd) ",
+        database_="neo4j")
+        # line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+        # print(line)
         return item
+
+
+
+# class JsonWriterPipeline:
+#     def open_spider(self, spider):
+#         self.file = open("items.jsonl", "w")
+
+#     def close_spider(self, spider):
+#         self.file.close()
+
+#     def process_item(self, item, spider):
+#         line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+#         self.file.write(line)
+#         return item
