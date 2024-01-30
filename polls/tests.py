91ad628bece32e4943133d0a1a8aee9a19814ffe
PaulinaFormas <131801151+PaulinaFormas@users.noreply.github.com>
2024-01-30 00:47:52 +0000

    bfddbg

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,36 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
+from datetime import datetime
+import re
 
-# Create your tests here.
+class ViewTests(TestCase):
 
+    
+    def test_index_view(self):
+        url = "http://127.0.0.1:8000/polls/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
+
+
+    def test_prowadzacy_view(self):
+        url = "http://127.0.0.1:8000/polls/prow"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # Odczytanie zawartości odpowiedzi jako JSON z uwzględnieniem kodowania UTF-8
+        content = response.content.decode('utf-8')
+        try:
+            data = json.loads(content)
+        except json.JSONDecodeError:
+            self.fail("Odpowiedź nie jest poprawnym JSON-em")
+        finally:
+            print("URL prowadzący działa poprawnie")
+    
+
+    def test_plan_view(self):
+        url = "http://127.0.0.1:8000/polls/plan/stud/js?grupa=WCY20IK1S0"
+        print(url)
+        response = self.client.get(url, data = {'grp':'WCY20IK1S0'})
+        self.assertEqual(response.status_code, 200)
+        
--- proj/settings.py
+++ proj/settings.py
@@ -77,6 +77,16 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
